1152436a8f3c649ef2cf276606c1485dc9195db7
Mohamed Ali Ben Alaya <mohamedali.benalaya@esprit.tn>
2019-06-20 13:50:38 +0100

    changement d'etat reservation

--- src/main/java/us/sqoin/hotpital/generators/ReservateGenerator.java
+++ src/main/java/us/sqoin/hotpital/generators/ReservateGenerator.java
@@ -7,8 +7,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import us.sqoin.hotpital.modal.Medecin;
 import us.sqoin.hotpital.modal.Reservation;
@@ -17,16 +20,21 @@ public class ReservateGenerator {
 
 	private static Reservation reservation1;
 	private static Reservation reservation2;
+	private static Reservation reservation3; 
 	private static ArrayList<Reservation> listResev = new ArrayList<Reservation>();
 	private static int i=0;
 	
 	static {
+	
+				
 
 		reservation1 = new Reservation(1l, HotelGenerator.getHotel1().getName(), PatientGenerator.getP1().getFirstName(), "Zarrouk", 2, new Date(),new Date(),0,"2019-06-24","2019-06-27");
-	    reservation2 = new Reservation(2l, "Mouradi", "Fathi", "Zarrouk", 1, new Date(), new Date(), 0,"2019-07-29","2019-07-09");
+	    reservation2 = new Reservation(2l, "Mouradi", "Fathi", "Zarrouk", 1, new Date(), new Date(), 1,"2019-07-29","2019-07-09");
+	    // reservation3 = new Reservation(2l, "Mouradi", "Fathi", "Zarrouk", 1, new Date(), new Date(), 1,"2019-07-29","2019-07-09");
 
 	    listResev.add(reservation1);
 		listResev.add(reservation2);
+		//listResev.add(reservation3);
 	}
 	
 	
@@ -181,9 +189,102 @@ public class ReservateGenerator {
 	}
 
 
+	public static List<Reservation> SortListReservationsByDateAsc()
+	{
+		List<Reservation> reservations =getDefaultReservations() ;
+		Collections.sort(reservations, new Comparator<Reservation>() {
+			  @Override
+			  public int compare(Reservation r1, Reservation r2) {
+				  Date parse = null;
+				  Date parse2=null;
+			        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+				  try {
+					parse = formatter.parse(r1.getDate_Deb());
+					parse2 = formatter.parse(r2.getDate_Deb());
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+						  
+			    return parse.compareTo(parse2);
+			  }
+			});
+		return reservations;
+	}
+	
+	public static List<Reservation> SortListReservationsByDateDesc()
+	{
+		List<Reservation> reservations =getDefaultReservations() ;
+		Collections.sort(reservations, new Comparator<Reservation>() {
+			  @Override
+			  public int compare(Reservation r1, Reservation r2) {
+				  Date parse = null;
+				  Date parse2=null;
+			        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+				  try {
+					parse = formatter.parse(r1.getDate_Deb());
+					parse2 = formatter.parse(r2.getDate_Deb());
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+						  
+			    return parse.compareTo(parse2);
+			  }
+			});
+		Collections.reverse(reservations);
+		return reservations;
+	}
+	
+	public static boolean ReservationCancelledByMed(int id)
+	{
+		List<Reservation> arf = getDefaultReservations();
+		
+		for (Reservation s : arf) {
+			if(s.getId()==id )
+			{
+				s.setEtat(3);
+				System.out.println(arf);
+				return true;
+			}
+		
+		}
+		return false;
+		
+	}
+	
+	public static boolean ReservationAcceptedByHot(int id)
+	{
+		List<Reservation> arf = getDefaultReservations();
+		
+		for (Reservation s : arf) {
+			if(s.getId()==id )
+			{
+				s.setEtat(1);
+				System.out.println(arf);
+				return true;
+			}
+		
+		}
+		return false;
+		
+	}
 
-	private static Object generateNotificationFromReservation(Reservation v) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public static boolean ReservationRefusedByHot(int id)
+	{
+		List<Reservation> arf = getDefaultReservations();
+		
+		for (Reservation s : arf) {
+			if(s.getId()==id )
+			{
+				s.setEtat(2);
+				System.out.println(arf);
+				return true;
+			}
+		
+		}
+		return false;
+		
 	}
 }
