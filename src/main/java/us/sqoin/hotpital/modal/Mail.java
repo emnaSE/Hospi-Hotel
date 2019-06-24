a42af9dba1092269082d7fdd0bf55febc29785aa
Mohamed Ali Ben Alaya <mohamedali.benalaya@esprit.tn>
2019-06-24 16:58:44 +0100

    adding image

---
+++ src/main/java/us/sqoin/hotpital/modal/Mail.java
@@ -0,0 +1,62 @@
+package us.sqoin.hotpital.modal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+
+@SpringBootApplication
+public class Mail  {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+   
+
+   public  void sendEmail() {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("dalialaya2@gmail.com");
+
+        msg.setSubject("It works");
+        msg.setText("Hello Worldsssssss \n Spring Boot Email");
+
+        javaMailSender.send(msg);
+
+    }
+
+   public void sendEmailWithAttachment() throws MessagingException, IOException {
+
+        MimeMessage msg = javaMailSender.createMimeMessage();
+
+        // true = multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(msg, true);
+        helper.setTo("1@gmail.com");
+
+        helper.setSubject("Testing from Spring Boot");
+
+        // default = text/plain
+        //helper.setText("Check attachment for image!");
+
+        // true = text/html
+        helper.setText("<h1>Check attachment for image!</h1>", true);
+
+        //FileSystemResource file = new FileSystemResource(new File("classpath:android.png"));
+
+        //Resource resource = new ClassPathResource("android.png");
+        //InputStream input = resource.getInputStream();
+
+        //ResourceUtils.getFile("classpath:android.png");
+
+        helper.addAttachment("my_photo.png", new ClassPathResource("android.png"));
+
+        javaMailSender.send(msg);
+
+    }
+}
+
